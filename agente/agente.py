8c1a46aedd55a07f44cbe52b025ce0e69b940272
Mario Jr <mariogasparoni@gmail.com>
2013-11-06 17:21:12 -0200

    Uma versao de escalar ja funciona. Ver agora como adicionar objetos que nao necessariamente sejam escalares, mas sim objetos raizes de outros escalares. No nosso caso, devemos criar um novo para o objeto general(smart.1) cards (smart.2), dentre  outros

--- agente/agente.py
+++ agente/agente.py
@@ -1,6 +1,8 @@
+import sys
 from pysnmp.entity import engine, config
 from pysnmp.entity.rfc3413 import cmdrsp, context
 from pysnmp.carrier.asynsock.dgram import udp
+from pysnmp.proto.api import v1
 
 # Create SNMP engine with autogenernated engineID and pre-bound
 # to socket transport dispatcher
@@ -22,6 +24,7 @@ config.addSocketTransport(
 # operations.
 config.addV1System(snmpEngine, 'my-read-area', 'public')
 config.addV1System(snmpEngine, 'my-write-area', 'private')
+config.addV1System(snmpEngine, 'my-mibsmart-area', 'public') #adiciona comunidade publica
 
 # Allow full MIB access for this user / securityModels at VACM
 config.addVacmUser(snmpEngine, 1, 'my-read-area',
@@ -29,8 +32,49 @@ config.addVacmUser(snmpEngine, 1, 'my-read-area',
 config.addVacmUser(snmpEngine, 1, 'my-write-area',
                    'noAuthNoPriv', (1,3,6,1,2,1), (1,3,6,1,2,1))
 
+# Permite usuarios consultarem nossa MIB
+config.addVacmUser(snmpEngine, 1, 'my-mibsmart-area', 'noAuthNoPriv', (1,3,6,5))
+
+
 # Get default SNMP context this SNMP engine serves
 snmpContext = context.SnmpContext(snmpEngine)
+
+
+
+# --- create custom Managed Object Instance ---
+
+mibBuilder = snmpContext.getMibInstrum().getMibBuilder()
+
+#MibScalar eh a classe que representara o nodo(raiz) escalar,
+#MibScalarInstance eh a classe que representa a instancia desse valor
+MibScalar, MibScalarInstance = mibBuilder.importSymbols(
+    'SNMPv2-SMI', 'MibScalar', 'MibScalarInstance'
+) 
+
+#Aqui definimos a classe para nosso valor de instancia. Obs.: ela extende
+#a classe de representacao de instancias do pysnmp
+class MyStaticMibScalarInstance(MibScalarInstance):
+    def getValue(self, name, idx):
+        return self.getSyntax().clone(
+            'Python %s running on a %s platform' % (sys.version, sys.platform)
+        )
+
+#aqui exportamos o nosso objeto (instanciado a partir de MibScalar)
+mibBuilder.exportSymbols(
+  'MIB-SMART', MibScalar((1,3,6,5,1), v1.OctetString()),
+              MyStaticMibScalarInstance((1,3,6,5,1), (0,), v1.OctetString())
+)
+
+# --- end of Managed Object Instance initialization ----
+
+
+
+#---MIB UFRGS --#
+
+    #Criar agora um nodo raiz, que nao seja escalar (MibScalar)
+
+
+#---------------#
 
 # Register SNMP Applications at the SNMP engine for particular SNMP context
 cmdrsp.GetCommandResponder(snmpEngine, snmpContext)
@@ -39,7 +83,6 @@ cmdrsp.NextCommandResponder(snmpEngine, snmpContext)
 
 # Register an imaginary never-ending job to keep I/O dispatcher running forever
 snmpEngine.transportDispatcher.jobStarted(1)
-
 # Run I/O dispatcher which would receive queries and send responses
 try:
     snmpEngine.transportDispatcher.runDispatcher()
