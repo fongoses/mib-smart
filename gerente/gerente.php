d277cfb4667acf9ac73dd39679ff30a330c72b92
Mario Jr <mariogasparoni@gmail.com>
2013-11-12 12:50:33 -0200

    Adicionada varial agenteIP para facilitar portabilidade

--- gerente/gerente.php
+++ gerente/gerente.php
@@ -1,16 +1,17 @@
 <?php
+	$agenteIP="192.168.0.105";
 
     echo "<h1> Dados do Fabricante do dispositivo</h1>";
-    $generalGet_1 = snmpget("192.168.0.105", "private", "1.3.6.1.4.1.12619.1.1.1.0");
+    $generalGet_1 = snmpget($agenteIP, "private", "1.3.6.1.4.1.12619.1.1.1.0");
     $general_1=str_replace("\"","",explode(": ",$generalGet_1)[1]);
 
-    $generalGet_2 = snmpget("192.168.0.105", "private", "1.3.6.1.4.1.12619.1.1.2.0");
+    $generalGet_2 = snmpget($agenteIP, "private", "1.3.6.1.4.1.12619.1.1.2.0");
     $general_2=str_replace("\"","",explode(": ",$generalGet_2)[1]);
 
-    $generalGet_3 = snmpget("192.168.0.105", "private", "1.3.6.1.4.1.12619.1.1.3.0");
+    $generalGet_3 = snmpget($agenteIP, "private", "1.3.6.1.4.1.12619.1.1.3.0");
     $general_3=str_replace("\"","",explode(": ",$generalGet_3)[1]);
 
-    $generalGet_4 = snmpget("192.168.0.105", "private", "1.3.6.1.4.1.12619.1.1.4.0");
+    $generalGet_4 = snmpget($agenteIP, "private", "1.3.6.1.4.1.12619.1.1.4.0");
     $general_4=str_replace("\"","",explode(": ",$generalGet_4)[1]);
     
     
